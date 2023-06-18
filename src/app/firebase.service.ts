54e5541bbc76ec04d8d28f3c247514f6bd8098d7
suleymankahraman <suleymankahraman1993@gmail.com>
2023-06-18 21:57:46 +0300

    init

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -9,47 +9,47 @@ import {
   doc,
   docData,
   orderBy,
-  query,} from '@angular/fire/firestore';
+  query,
+} from '@angular/fire/firestore';
 
+export interface demirbaslar {
+  id?: string;
+  ad?: string;
+  tur?: string;
+  adet?: string;
+  kayitTarih?: string;
+}
 
-  export interface demirbaslar{
-    id?: string;
-    ad?: string;
-    tur?:string;
-    adet?: string;
-    kayitTarih?: string; 
-  }
+export interface personeller {
+  id?: string;
+  ad?: string;
+  soyad?: string;
+  sicilNo?: number;
+}
 
-  export interface personeller{
-    id?: string;
-    ad?: string;
-    soyad?:string;
-    sicilNo?: number;
-  }
-
-  export interface yoneticiler{
-    email?: string;
-    password?: string;
-  }
+export interface yoneticiler {
+  email?: string;
+  password?: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseService {
-
   constructor(public fireStore: Firestore) {}
 
-  listDemirbasByID(personelId){
-    const demirbaslar = collection(this.fireStore, `personel/${personelId}/demirbaslar`);
+  listDemirbasByID(personelId) {
+    const demirbaslar = collection(
+      this.fireStore,
+      `personel/${personelId}/demirbaslar`
+    );
     //const siraliDemirbaslar = query(demirbaslar, orderBy('ad', 'asc'));
-    return collectionData(demirbaslar, {idField: 'id'}); 
+    return collectionData(demirbaslar, { idField: 'id' });
   }
 
-  listPersonel(){
-
+  listPersonel() {
     const personel = collection(this.fireStore, 'personel');
-    return collectionData(personel, {idField: 'id'});
-
+    return collectionData(personel, { idField: 'id' });
   }
 
   // getPersonelById(personelId){
@@ -58,20 +58,33 @@ export class FirebaseService {
   //   return collectionData()
   // }
 
-  addNewDemirbasById(demirbas: demirbaslar, personelId ){
-
-    const addDemirbas = collection(this.fireStore, `personel/${personelId}/demirbaslar`); 
-    return addDoc(addDemirbas, demirbas); 
+  addNewDemirbasById(demirbas: demirbaslar, personelId) {
+    const addDemirbas = collection(
+      this.fireStore,
+      `personel/${personelId}/demirbaslar`
+    );
+    return addDoc(addDemirbas, demirbas);
   }
 
-  addNewPersonel(personel: personeller){
-
+  addNewPersonel(personel: personeller) {
     const addPersonel = collection(this.fireStore, 'personel');
     console.log(addPersonel);
     debugger;
     return addDoc(addPersonel, personel);
   }
 
+  updatePersonel(newPersonel: personeller) {
+    const currentPersonel = doc(this.fireStore, `personel/${newPersonel.id}`);
+    return updateDoc(currentPersonel, {
+      ad: newPersonel.ad,
+      soyad: newPersonel.soyad,
+      sicilNo: newPersonel.sicilNo,
+    });
+  }
 
+  deletePersonelById(personelId){
+
+    const personel = doc(this.fireStore, `personel/${personelId}`);
+    return deleteDoc(personel);
+  }
 }
-
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService, demirbaslar, personeller } from '../firebase.service';
-import { AlertController, ModalController } from '@ionic/angular';
+import {
+  AlertController,
+  ModalController,
+  ToastController,
+} from '@ionic/angular';
 import { Router } from '@angular/router';
 import { Timestamp } from '@angular/fire/firestore';
 import { ModalPage } from '../modal/modal.page';
@@ -12,11 +16,14 @@ import { ModalPage } from '../modal/modal.page';
 })
 export class HomePage implements OnInit {
   personeller?: personeller[];
+  handlerMessage = '';
+  roleMessage = '';
 
   constructor(
     public modalController: ModalController,
     public fireService: FirebaseService,
-    public alertController: AlertController
+    public alertController: AlertController,
+    public toastController: ToastController
   ) {}
 
   ngOnInit() {
@@ -26,10 +33,7 @@ export class HomePage implements OnInit {
     });
   }
 
-  async updatePersonel(personel:personeller) {
-
-    console.log(personel);
-    debugger;
+  async updatePersonel(personel: personeller) {
     const alert = await this.alertController.create({
       header: 'Personel Bilgileri',
       inputs: [
@@ -37,25 +41,24 @@ export class HomePage implements OnInit {
           name: 'ad',
           placeholder: 'İsim giriniz...',
           type: 'text',
-          value: personel.ad
+          value: personel.ad,
         },
         {
           name: 'soyad',
           placeholder: 'Soyisim giriniz...',
           type: 'text',
-          value:personel.soyad
+          value: personel.soyad,
         },
         {
           name: 'sicilNo',
           placeholder: '4 Haneli Sicil No giriniz...',
           type: 'number',
-          value:personel.sicilNo,
-          min:1,
+          value: personel.sicilNo,
+          min: 1,
           max: 9999,
           attributes: {
-            length: 4, 
+            length: 4,
           },
-        
         },
       ],
       buttons: [
@@ -63,21 +66,28 @@ export class HomePage implements OnInit {
           text: 'Vazgeç',
           role: 'Cancel',
         },
-        // {
-        //   text: 'Ekle',
-        //   handler: (res) => {
-        //    const newPersonel = {
-        //     ad:res.ad,
-        //     soyad:res.soyad,
-        //     sicilNo:res.sicilNo
-        //    }
-        //     this.fireService.addNewPersonel(newPersonel); 
-        //   },
-        // },
+        {
+          text: 'Güncelle',
+          handler: async (res) => {
+            if (res.sicilNo > 9999) {
+              const toast = await this.toastController.create({
+                message: 'Sicil No 4 Haneli Olmalıdır!',
+                duration: 2000,
+                color: 'danger',
+              });
+              toast.present();
+              this.updatePersonel(personel);
+          }else{
+            personel.ad = res.ad;
+            personel.soyad = res.soyad;
+            personel.sicilNo = res.sicilNo;
+            this.fireService.updatePersonel(personel);
+          }
+        },
+      }
       ],
-
-    })
-      await alert.present();
+    });
+    await alert.present();
   }
 
   async addNewPersonel() {
@@ -98,12 +108,6 @@ export class HomePage implements OnInit {
           name: 'sicilNo',
           placeholder: '4 Haneli Sicil No giriniz...',
           type: 'number',
-          min:1,
-          max: 9999,
-          attributes: {
-            length: 4, 
-          },
-        
         },
       ],
       buttons: [
@@ -113,13 +117,23 @@ export class HomePage implements OnInit {
         },
         {
           text: 'Ekle',
-          handler: (res) => {
-           const newPersonel = {
-            ad:res.ad,
-            soyad:res.soyad,
-            sicilNo:res.sicilNo
-           }
-            this.fireService.addNewPersonel(newPersonel); 
+          handler: async (res) => {
+            if (res.sicilNo > 9999) {
+              const toast = await this.toastController.create({
+                message: 'Sicil No 4 Haneli Olmalıdır!',
+                duration: 2000,
+                color: 'danger',
+              });
+              toast.present();
+              this.addNewPersonel();
+            } else {
+              const newPersonel = {
+                ad: res.ad,
+                soyad: res.soyad,
+                sicilNo: res.sicilNo,
+              };
+              this.fireService.addNewPersonel(newPersonel);
+            }
           },
         },
       ],
@@ -134,5 +148,39 @@ export class HomePage implements OnInit {
     });
 
     modal.present();
+  }
+
+  async deleteAllert(id: any) {
+    const alert = await this.alertController.create({
+      header: 'Silmek İstediğinizden Emin misiniz?',
+      buttons: [
+        {
+          text: 'Vazgeç',
+          role: 'Cancel',
+          handler: async () => {
+            const toast = await this.toastController.create({
+              message: 'Silme İşlemi İptal Edildi!',
+              duration: 2000,
+              color: 'danger',
+            });
+            toast.present();
+          },
+        },
+        {
+          text: 'Evet',
+          role: 'confirm',
+          handler: async () => {
+            this.fireService.deletePersonelById(id);
+            const toast = await this.toastController.create({
+              message: 'Silme İşlemi Başarılı!',
+              duration: 2000,
+              color: 'success',
+            });
+            toast.present();
+          },
+        },
+      ],
+    });
+    await alert.present();
   }
 }
